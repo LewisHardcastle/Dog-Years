86859d681fde8aa79f01027a8be3a6ef53b78754
Lewis Hardcastle <106673013+LewisHardcastle@users.noreply.github.com>
2022-08-01 14:54:06 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+//Variable initialization
+const myAge = 25;
+let earlyYears = 2;
+earlyYears = earlyYears * 10.5;
+//Use arithmatic operators to assign variables
+let laterYears = myAge - 2;
+//Multiplication assignment operator
+laterYears *= 4;
+myAgeInDogYears = earlyYears + laterYears;
+myName = 'Lewis';
+//Use the built in string method to make all charecters lower case
+console.log(myName.toLowerCase());
+//Use string interpolationt to declare the variables value within a string that is printed
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
